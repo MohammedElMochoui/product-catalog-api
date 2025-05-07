961d5bf261d755d170039cd16f988d1cc02d7ef7
Mohammed <m.mochoui@gmail.com>
2025-05-07 16:50:19 +0200

    Added login response dto

--- src/main/java/com/example/product_catalog_api/user/controller/UserController.java
+++ src/main/java/com/example/product_catalog_api/user/controller/UserController.java
@@ -2,12 +2,13 @@ package com.example.product_catalog_api.user.controller;
 
 import com.example.product_catalog_api.user.dto.request.LoginRequestDTO;
 import com.example.product_catalog_api.user.dto.request.RegisterRequestDTO;
+import com.example.product_catalog_api.user.dto.response.LoginResponseDTO;
 import com.example.product_catalog_api.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("auth")
 public class UserController {
     private final UserService userService;
 
@@ -15,13 +16,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<String> getToken(@RequestBody LoginRequestDTO loginRequestDTO) {
+    @PostMapping("login")
+    public ResponseEntity<LoginResponseDTO> getToken(@RequestBody LoginRequestDTO loginRequestDTO) {
         String token = userService.loginUser(loginRequestDTO.username(), loginRequestDTO.password());
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
-    @PostMapping("/register")
+    @PostMapping("register")
     public ResponseEntity<String> registerUser(@RequestBody RegisterRequestDTO registerRequestDTO) {
         userService.registerUser(registerRequestDTO.username(), registerRequestDTO.password());
         return ResponseEntity.ok("User with username " + registerRequestDTO.username() + " registered successfully!");
---
+++ src/main/java/com/example/product_catalog_api/user/dto/response/LoginResponseDTO.java
@@ -0,0 +1,6 @@
+package com.example.product_catalog_api.user.dto.response;
+
+public record LoginResponseDTO(
+        String token
+) {
+}
